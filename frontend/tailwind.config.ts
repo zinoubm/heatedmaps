64db44aef6a45562169df091465071918ca7868f
zinoubm <zinne.eddine.2000@gmail.com>
2024-07-22 06:50:50 +0100

    changing background color

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -28,7 +28,7 @@ const config = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
-        orangeaccent: "#F8F7F4",
+        orangeaccent: "#FFF8EE",
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
